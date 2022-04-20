f48754b02eb7fae243ff25a81c71b53f54ff6155
Emirhan Demirci <emirhhan1905@hotmail.com>
2022-04-20 11:49:40 +0300

    switch-case

--- SwitchCase/Program.cs
+++ SwitchCase/Program.cs
@@ -6,39 +6,83 @@ namespace SwitchCase
     {
         static void Main(string[] args)
         {
-            string plaka = "";
-            Console.WriteLine("plaka kodunu giriniz");
-            plaka= Console.ReadLine();
+            #region örnek1
+            //string plaka = "";
+            //Console.WriteLine("plaka kodunu giriniz");
+            //plaka = Console.ReadLine();
 
-            if (plaka == "1" || plaka == "01")
-            {
-                Console.WriteLine("Adana");
-            }
-            else if (plaka == "2" || plaka == "02")
-            {
-                Console.WriteLine("Adıyaman");
-            }
-            else if (plaka == "3" || plaka == "03")
-            {
-                Console.WriteLine("Afyon");
-            }
+            //if (plaka == "1" || plaka == "01")
+            //{
+            //    Console.WriteLine("Adana");
+            //}
+            //else if (plaka == "2" || plaka == "02")
+            //{
+            //    Console.WriteLine("Adıyaman");
+            //}
+            //else if (plaka == "3" || plaka == "03")
+            //{
+            //    Console.WriteLine("Afyon");
+            //}
 
-            switch (plaka)
+            //switch (plaka)
+            //{
+            //    case "1":
+            //        Console.WriteLine("Adana");
+            //        break;
+            //    case "2":
+            //        Console.WriteLine("Adıyaman");
+            //        break;
+            //    case "3":
+            //        Console.WriteLine("Afyon");
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            //Console.WriteLine("kullanıcı:");
+            //string kullanıcı = Console.ReadLine();
+            
+            //switch (kullanıcı)
+            //{
+            //    case "Admin" or "admin":
+            //        Console.WriteLine("yönetim paneline yönlendiriliyorsunız");
+            //        break;
+            //    default:
+            //        break;
+
+            //}
+
+
+            #endregion
+
+
+            #region örnek 2
+
+            Console.WriteLine("Kullanici adi giriniz");
+            string kullaniciAdi = Console.ReadLine();
+
+            Console.WriteLine("parola giriniz");
+            string parola = Console.ReadLine();
+
+            switch (kullaniciAdi)
             {
-                case "1":
-                    Console.WriteLine("Adana");
-                    break;
-                case "2":
-                    Console.WriteLine("Adıyaman");
-                    break;
-                case "3":
-                    Console.WriteLine("Afyon");
+                case "bilgeadam":
+                    switch (parola)
+                    {
+                        case "1234":
+                            Console.WriteLine("kullanici adi ve parola doğrudur");
+                            break;
+                        default:
+                            Console.WriteLine("kullanıcı adı doğru parola yanlış");
+                            break;
+                    }
                     break;
                 default:
+                    Console.WriteLine("Kullanici adi dogru, parola yanlış");
                     break;
             }
-
+            #endregion
 
         }
     }
-}
+}     
